b2162b60ce5f4192dbdf13044d5dadfb3d81b4b3
JoseMPC <jomaplataca@ittepic.edu.mx>
2024-04-05 01:32:23 -0700

    Add/Fixed
    Se agregaron nuevas expresiones regulares y se modifcaron partes del codigo

--- src/analizadores/lexico.py
+++ src/analizadores/lexico.py
@@ -5,7 +5,8 @@ import re
 import codecs
 import os
 import sys
-tokens = {
+
+tokens = [
     'if',
     'then',
     'else',
@@ -68,13 +69,56 @@ tokens = {
     'AND',
     'NOT',
     'COMENTARIOS',
+    'COMENTARIOS_MULTILINEA',
+    'FIN_DE_INSTRUCCION',
+]
+
+
+#Se Reestructuro el código de t_ID para evitar el uso de if-else
+#Se utiliza un diccionario de terminos para facilitar el creciemiento de las palabras reservadas.
+reserved = {
+    'if': 'if',
+    'then': 'then',
+    'else': 'else',
+    'for': 'for',
+    'while': 'while',
+    'stop': 'stop',
+    'method': 'method',
+    'run': 'run',
+    'return': 'return',
+    'console': 'console',
+    'export': 'export',
+    'import': 'import',
+    'mbm': 'mbm',
+    'arm': 'arm',
+    'hand': 'hand',
+    'rotate': 'rotate',
+    'fng1': 'fng1',
+    'fng2': 'fng2',
+    'fng3': 'fng3',
+    'fng4': 'fng4',
+    'fng5': 'fng5',
+    'is': 'is',
+    'mov': 'mov',
+    'force': 'force',
+    'sensor': 'sensor',
+    'weight': 'weight',
+    'block': 'block',
+    'up': 'up',
+    'down': 'down',
+    'linkage': 'linkage',
+    'wrist': 'wrist',
+    'piRad': 'piRad',
+    'degree': 'degree',
+    'wait': 'wait',
 }
+
 
 #Expresiones regulares para Tokens Simples
 t_PLUS = r'\+'
 t_INCRE = r'\++'
-#t_DECRE = r'\--' #Posiblemente no jale
-t_MINUS = r'\-'
+#t_DECRE = r'\--' Tenemos que discutir como solucionar el decremento
+#t_MINUS = r'\-'  Igual a discuSION
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LT = r'<'
@@ -97,6 +141,10 @@ t_GTE = r'>='
 t_AND = r'AND'
 t_NOT = r'NOT'
 
+
+#Ignora espacios en blanco y tabulaciones
+t_ignore = ' \t'
+
 #Expresión Regular para Números decimales
 def t_DECIMAL(t):
     r'\d+\.\d+'
@@ -118,100 +166,44 @@ def t_BOOL(t):
 #Expresión Regular para identificadores (nombres de variables, funciones, etc.).
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    if t.value == 'if':
-        t.type == 'if'
-    elif t.value == 'then':
-        t.type == 'then'
-    elif t.value == 'else':
-        t.type == 'else'
-    elif t.value == 'for':
-        t.type == 'for'
-    elif t.value == 'while':
-        t.type == 'while'
-    elif t.value == 'stop':
-        t.type == 'stop'
-    elif t.value == 'method':
-        t.type == 'method'
-    elif t.value == 'run':
-        t.type == 'run'
-    elif t.value == 'return':
-        t.type == 'return'
-    elif t.value == 'console':
-        t.type == 'console'
-    elif t.value == 'export':
-        t.type == 'export'
-    elif t.value == 'import':
-        t.type == 'import'
-    elif t.value == 'mbm':
-        t.type == 'mbm'
-    elif t.value == 'arm':
-        t.type == 'arm'
-    elif t.value == 'hand':
-        t.type == 'hand'
-    elif t.value == 'rotate':
-        t.type == 'rotate'
-    elif t.value == 'fng1':
-        t.type == 'fng1'
-    elif t.value == 'fng2':
-        t.type == 'fng2'
-    elif t.value == 'fng3':
-        t.type == 'fng3'
-    elif t.value == 'fng4':
-        t.type == 'fng4'
-    elif t.value == 'fng5':
-        t.type == 'fng5'
-    elif t.value == 'is':
-        t.type == 'is'
-    elif t.value == 'mov':
-        t.type == 'mov'
-    elif t.value == 'force':
-        t.type == 'force'
-    elif t.value == 'sensor':
-        t.type == 'sensor'
-    elif t.value == 'weight':
-        t.type == 'weight'
-    elif t.value == 'block':
-        t.type == 'block'
-    elif t.value == 'up':
-        t.type == 'up'
-    elif t.value == 'down':
-        t.type == 'down'
-    elif t.value == 'linkage':
-        t.type == 'linkage'
-    elif t.value == 'wrist':
-        t.type == 'wrist'
-    elif t.value == 'piRad':
-        t.type == 'piRad'
-    elif t.value == 'degree':
-        t.type == 'degree'
-    elif t.value == 'wait':
-        t.type == 'wait'
-    elif t.value == 'ID':
-        t.type == 'NUMBER'
-    elif t.value == 'DECIMAL':
-        t.type == 'DECIMAL'
-    elif t.value == 'BOOL':
-        t.type == 'BOOL'
-    #elif t.value == 'PLUS': No se si van lo de las operaciones o ne
-       # t.type == 'PLUS' Tampoco se si va lo que serian los signos de agrupacion,
-    # Comas y todas esas vainas, queda por ver
-    elif t.value == 'AND':
-        t.type == 'AND'
-    elif t.value == 'NOT':
-        t.type == 'NOT'
+    t.type = reserved.get(t.value, 'ID')
     return t
+
+
+# Lista para almacenar los comentarios junto con su posición de inicio
+comments = []
 
 #Expresión Regular para comentarios
 def t_COMENTARIOS(t):
     r'\;.*'
-    pass
+    comments.append((t.value, t.lexer.lineno, t.lexpos))
+    return t
 
-#Ignora espacios en blanco y tabulaciones
-t_ignore = '\t'
+# Lista para almacenar los comentarios junto con su posición de inicio
+commentsML = []
+#Expresión regular para comentarios de multiple linea
+def t_COMENTARIOS_MULTILINEA(t):
+    r'<\-(?:[^-]|-(?!>))*\->'  # Comentarios de múltiples líneas entre <- y ->
+    t.lexer.lineno += t.value.count('\n')
+    commentsML.append((t.value, t.lexer.lineno, t.lexpos))
+    return t
+
+#Expresión para ignorar saltos de linea
+t_ignore_NEWLINE = r'\n'
+
+#Expresión para actualiar la linea
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+#Expresión Regular para fin de linea
+def t_FIN_DE_INSTRUCCION(t):
+    r'\$'
+    return t
 
 # Manejo de errores
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno} at column {t.lexpos}")
     t.lexer.skip(1)
 
 # Evitar la impresión de advertencias sobre tokens no utilizados
@@ -221,12 +213,20 @@ lex.errorlog = lex.NullLogger()
 lexer = lex.lex()
 
 codigo = """
-
-"""
+method run(){
+    ;Aquí mandas a llamar los métodos que llegues a crear
+    fng1 = 30$
+    <- Hola
+    como ta ->
+    AgarrarSoltar()$
+}"""
+# Evitar la impresión de advertencias sobre tokens no utilizados
+lex.errorlog = lex.NullLogger()
 
 # Pasar el código al analizador léxico
 lexer.input(codigo)
 
 # Tokenizar e imprimir los tokens
 for tok in lexer:
-    print(tok)
+    print(tok)  
+
