86239c86dd5c343b617446e92f9a1cf89d706075
JoseLinoDC <149347079+JoseLinoDC@users.noreply.github.com>
2024-05-21 23:22:43 -0700

    Actualizaciones
    Muestra una tabla estática, se modificó la ventana del analizador léxico, no se ha arreglado el problema del color de los tokens y el tamaño de la letra.

--- src/analizadores/Main_view.py
+++ src/analizadores/Main_view.py
@@ -2,23 +2,14 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox as mb
-from lexico import tokens,reserved,lexer
+from lexico import tokens, reserved, lexer, descriptions
 from sintactico import parser, yacc
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter import ttk
 
+# Estructura Visual del compilador y funciones basicas
 
-#Estructura Visual del compilador y funciones basicas
-
-#En la siguiente clase creamos un Scroll que tenga numeros al costado que contabiliza los renglones
-#como es en los Compiladores habituales
-'''
-ACLARACION:
-la clase ScrollTextWithLineNumbers simula como cuadro de codigo que simula al del cualquier compilador
-pero existe un problema y es que no funciona tan bien como se esperaba ya que apesar de que se modifique
-sus dimensiones la numeracion seguira aumentando más no se mostraran. Así que si ven que su limite es
-24 lineas, apesar que el codigo sea de más renglones, no se pudo corregir este bug.
-'''
 class ScrollTextWithLineNumbers(Frame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)  # Llama al constructor de la clase base (Frame)
@@ -83,40 +74,95 @@ class ScrollTextWithLineNumbers(Frame):
         self.text_widget.delete(1.0, 'end')  # Borra todo el texto en el widget
         self._update_line_numbers()  # Actualiza la numeración de líneas
 
-#LISTA DE FUNCIONES EXISTENTES
+# Variables para almacenar las referencias de las ventanas
+lexico_window = None
+sintactico_window = None
+tabla_window = None
 
-def analisisSintactico():
+def cerrar_ventana(window):
+    if window is not None:
+        window.destroy()
+        window = None
+    return window
+'''
+def mostrarAnalisisLexico2(tokens):
+    global lexico_window
+    lexico_window = cerrar_ventana(lexico_window)
+    lexico_window = Toplevel()
+    lexico_window.title("Análisis Léxico")
+
+    # Crear un Text widget con un Scrollbar
+    text_area = Text(lexico_window, wrap='word')
+    scrollbar = Scrollbar(lexico_window, command=text_area.yview)
+    text_area.configure(yscrollcommand=scrollbar.set)
+
+    # Configurar la posición de los widgets
+    text_area.pack(side='left', fill='both', expand=True)
+    scrollbar.pack(side='right', fill='y')
+    text_area.config(state="normal")  # Cambiar el estado a normal para permitir la edición
+    text_area.delete(1.0, END)  # Borrar el contenido previo
+    for token in tokens:
+        token_type, token_value, token_lineno, token_lexpos = token
+        text_area.insert(INSERT, f'Tipo: {token_type}, Valor: {token_value}, Ren: {token_lineno}, Col: {token_lexpos}\n')  # Insertar cada token en una nueva línea
+    text_area.config(state="disabled")  # Volver a deshabilitar la edición
+'''
+
+def mostrarAnalisisLexico2(tokens):
+    global lexico_window
+    lexico_window = cerrar_ventana(lexico_window)  # Cerrar la ventana existente si hay una
+
+    # Crear una nueva ventana
+    lexico_window = Toplevel()
+    lexico_window.title("Análisis Léxico")
+
+    # Crear un Treeview widget con columnas
+    tree = ttk.Treeview(lexico_window, columns=('Tipo', 'Valor', 'Renglon', 'Columna'), show='headings')
+
+    # Configurar encabezados de las columnas
+    tree.heading('Tipo', text='Tipo')
+    tree.heading('Valor', text='Valor')
+    tree.heading('Renglon', text='Renglon')
+    tree.heading('Columna', text='Columna')
+
+    # Configurar tamaño de las columnas
+    tree.column('Tipo', minwidth=0, width=100)
+    tree.column('Valor', minwidth=0, width=150)
+    tree.column('Renglon', minwidth=0, width=70)
+    tree.column('Columna', minwidth=0, width=70)
+
+    # Insertar datos en el Treeview widget
+    for token in tokens:
+        token_type, token_value, token_lineno, token_lexpos = token
+        tree.insert('', tk.END, values=(token_type, token_value, token_lineno, token_lexpos))
+
+    # Crear un Scrollbar y asociarlo al Treeview
+    scrollbar = Scrollbar(lexico_window, orient=tk.VERTICAL, command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+
+    # Configurar la posición de los widgets
+    tree.pack(side='left', fill='both', expand=True)
+    scrollbar.pack(side='right', fill='y')
+
+
+def analisisLexico():
     cadena = scroll_text_widget.get_text()
     if len(cadena) > 0:
-        try:
-            resultado = parser.parse(cadena)
-            mostrarAnalisisSintactico2(resultado)
-        except yacc.YaccError as e:
-            print("Error durante el análisis sintáctico:", e)
-            mb.showerror("Error", str(e))
+        lexer.input(cadena)
+        a_tok = []
+        for tok in lexer:
+            a_tok.append((tok.type, tok.value, tok.lineno, tok.lexpos))
+        mostrarAnalisisLexico2(a_tok)
     else:
-        mb.showwarning("ERROR", "Debes escribir código")
-       
-'''
-def mostrarAnalisisSintactico(data):
-    scrollAnalisis.config(state="normal")  # Cambiar el estado a normal para permitir la edición
-    scrollAnalisis.delete(1.0, END)  # Borrar el contenido previo
-    if isinstance(data, (int, float)):
-        scrollAnalisis.insert(END, str(data) + '\n')
-    else:
-        for item in data:
-            scrollAnalisis.insert(END, str(item) + '\n')
-    scrollAnalisis.config(state="disabled")  # Volver a deshabilitar la edición      
-'''
+        mb.showwarning("ERROR", "Debes escribir código")
 
-#Mostrar el analisis sintactico en un ventana aparte 
 def mostrarAnalisisSintactico2(data):
-    # Crear una nueva ventana
-    nueva_ventana = tk.Toplevel()
-    nueva_ventana.title("Análisis Sintáctico")
+    global sintactico_window
+    sintactico_window = cerrar_ventana(sintactico_window)
+    sintactico_window = tk.Toplevel()
+    sintactico_window.title("Análisis Sintáctico")
     # Crear un Text widget con un Scrollbar
-    text_area = Text(nueva_ventana, wrap='word')
-    scrollbar = Scrollbar(nueva_ventana, command=text_area.yview)
+    text_area = Text(sintactico_window, wrap='word')
+    scrollbar = Scrollbar(sintactico_window, command=text_area.yview)
     text_area.configure(yscrollcommand=scrollbar.set)
 
     # Configurar la posición de los widgets
@@ -133,43 +179,45 @@ def mostrarAnalisisSintactico2(data):
             text_area.insert(END, str(item) + '\n')
     text_area.config(state="disabled")  # Volver a deshabilitar la edición
 
+def analisisSintactico():
+    cadena = scroll_text_widget.get_text()
+    if len(cadena) > 0:
+        try:
+            resultado = parser.parse(cadena)
+            mostrarAnalisisSintactico2(resultado)
+        except yacc.YaccError as e:
+            print("Error durante el análisis sintáctico:", e)
+            mb.showerror("Error", str(e))
+    else:
+        mb.showwarning("ERROR", "Debes escribir código")
 
-#Mostrar el analisis lexico en un ventana aparte 
-def mostrarAnalisisLexico2(tokens):
-    # Crear una nueva ventana
-    nueva_ventana = tk.Toplevel()
-    nueva_ventana.title("Análisis Léxico")
+def tablaEstatica():
+    global tabla_window
+    tabla_window = cerrar_ventana(tabla_window)
+    tabla_window = tk.Toplevel()
+    tabla_window.title("Descripciones de Palabras Reservadas")
+    tabla_window.geometry('600x400')
+    tabla_window.resizable(True,False)
 
-    # Crear un Text widget con un Scrollbar
-    text_area = Text(nueva_ventana, wrap='word')
-    scrollbar = Scrollbar(nueva_ventana, command=text_area.yview)
-    text_area.configure(yscrollcommand=scrollbar.set)
+    table = ttk.Treeview(tabla_window, columns=("Palabra Reservada", "Descripción"))
+    table.heading("Palabra Reservada", text="Palabra Reservada")
+    table.heading("Descripción", text="Descripción")
 
-    # Configurar la posición de los widgets
-    text_area.pack(side='left', fill='both', expand=True)
-    scrollbar.pack(side='right', fill='y')
+    table.column("#0", width=0, minwidth=0)  # Adjust column widths as needed
+    table.column("Palabra Reservada", width=200, minwidth=200)
+    table.column("Descripción", width=400, minwidth=400)
 
-    # Insertar datos en el Text widget
-    text_area.config(state="normal")  # Cambiar el estado a normal para permitir la edición
-    text_area.delete(1.0, END)  # Borrar el contenido previo
-    for token in tokens:
-        token_type, token_value, token_lineno, token_lexpos = token
-        text_area.insert(INSERT, f'Tipo: {token_type}, Valor: {token_value}, Ren: {token_lineno}, Col: {token_lexpos}\n')  # Insertar cada token en una nueva línea
-    text_area.config(state="disabled")  # Volver a deshabilitar la edición
+    # Insertar datos en el Treeview
+    for word, description in descriptions.items():
+        table.insert("", tk.END, values=(word, description))
 
-'''
-def mostrarAnalisisLexico(tokens):
-    scrollAnalisis.config(state="normal")  # Cambiar el estado a normal para permitir la edición
-    scrollAnalisis.delete(1.0, END)  # Borrar el contenido previo
-    for token in tokens:
-        token_type, token_value, token_lineno, token_lexpos = token
-        scrollAnalisis.insert(INSERT, f'Tipo: {token_type}, Valor: {token_value}, Ren: {token_lineno}, Col: {token_lexpos}\n')  # Insertar cada token en una nueva línea
-    scrollAnalisis.config(state="disabled")  # Volver a deshabilitar la edición
-'''
-'''
- token_type, token_value, token_lineno, token_lexpos = token
- scrollAnalisis.insert(INSERT, f'Tipo: {token_type}, Valor: {token_value}, Ren: {token_lineno}, Col: {token_lexpos}\n')
-'''
+    # Usar una Scrollbar para el desplazamiento vertical
+    scrollbar = ttk.Scrollbar(tabla_window, orient="vertical", command=table.yview)
+    table.configure(yscrollcommand=scrollbar.set)
+
+    # Empaquetar los widgets
+    scrollbar.pack(side="right", fill="y")
+    table.pack(fill="both", expand=True)
 
 def AbrirArchivos():
     filename = filedialog.askopenfilename(initialdir="/",
@@ -185,7 +233,6 @@ def AbrirArchivos():
                 scroll_text_widget._update_line_numbers()
         else:
             mb.showerror("Error", "El archivo seleccionado no es de tipo .txt o .ht")
-
 
 def GuardarComo():
     filename = filedialog.asksaveasfilename(initialdir="/", title="Guardar como",
@@ -213,7 +260,6 @@ def Guardar():
     else:
         GuardarComo()
 
-
 def NuevoArchivo():
     root.title("Compilador Python")
     scroll_text_widget.clear_scroll_text()
@@ -221,26 +267,39 @@ def NuevoArchivo():
 def AunSinAgregar():
     mb.showerror("Atención","Aun no se agrega esta funcion al compilador.")
 
-def analisisLexico():
-    cadena = scroll_text_widget.get_text()
-    if len(cadena) > 0:
-        lexer.input(cadena)
-        a_tok = []
-        for tok in lexer:
-            a_tok.append((tok.type, tok.value, tok.lineno, tok.lexpos))
-        mostrarAnalisisLexico2(a_tok)
-    else:
-        mb.showwarning("ERROR", "Debes escribir código")
+def cambiar_tamaño_letra(size):
+    scroll_text_widget.text_widget.config(font=("Console", size))
+    if scrollAnalisis:
+        scrollAnalisis.config(font=("Console", size))
 
-#a_tok.append((tok.type, tok.value, tok.lineno, tok.lexpos))
-#Configuracion del entorno
+
+def Ventana2(data,title):
+    vt2 = Tk()
+    vt2.title(title)
+    vt2.geometry('400x400')
+
+    canvas = Canvas(vt2)
+    scroll_y = Scrollbar(vt2, orient="vertical", command=Canvas.yview)
+
+    frame = Frame(Canvas)
+
+    i=0;
+    for i in range(len(data)):
+        e = Label(frame,text=data[i])
+        e.grid(row=i,column=2)
+
+    canvas.create_window(0,0, anchor='nw', Windows=frame)
+    canvas.update_idletasks()
+    canvas.configure(scrollregion=canvas.bbox('all'),yscrollcommand=scroll_y.set)
+    canvas.pack(fill='both', expand=True, side='left')
+    vt2.mainloop()
+
+# Funciones adicionales (AbrirArchivos, GuardarComo, Guardar, NuevoArchivo, etc.) se mantienen igual
 root = Tk()
-root.resizable(False,False) # Con esto denegamos que se ajuste el tamaño de la ventana de largo y ancho
+root.resizable(FALSE,FALSE) # Con esto denegamos que se ajuste el tamaño de la ventana de largo y ancho
 root.geometry("924x596") #definimos las dimesiones de la ventana
 root.title("Compilador Python") #Titulo de la ventana
 
-#En este punto realizamos una serie de calculos para que cada que se ejecute este codigo
-#la pantalla del compilador inicie desde el centro de la pantalla
 wtotal = root.winfo_screenwidth()
 htotal = root.winfo_screenheight()
 wventana = 930
@@ -249,77 +308,38 @@ pwidth = round(wtotal/2-wventana/2)
 pheight = round(htotal/2-hventana/2)
 root.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
 
-#Mandamos a llamar a ScrollTextWithLine y definimos donde estará
 scroll_text_widget = ScrollTextWithLineNumbers(root)
 scroll_text_widget.grid(row=1,column=0,padx=10,pady=10)
 
-#Este codigo medio funciona
-def mostrarResultados(tokens,reserved,lexer):
-    sintactico_window = Toplevel(root)
-    sintactico_window.title("Analisis Sintactico")
-    sintactico_window.geometry("450x450")
-
-    sintactico_scroll = Scrollbar(sintactico_window)
-    sintactico_scroll.pack(side=RIGHT, fill=Y)
-
-    sintactico_text = Text(sintactico_window, yscrollcommand=sintactico_scroll.set)
-    sintactico_text.pack(expand=True, fill=BOTH)
-
-    for asin in tokens,reserved,lexer:
-        idMethod,tipo,idParam,bloque = asin
-        # Por ejemplo:token_type, token_value, = token
-        sintactico_text.insert(INSERT, f" {idMethod} {tipo} {idParam} {{\n{bloque}\n}}")
-        # sintactico_text.insert(INSERT, f"{token_type} {token_value}\n")
-        pass  # Esto es solo para mostrar un marcador de posición
-    sintactico_scroll.config(command=sintactico_text.yview)
-
-#Las siguientes lineas comentadas son para que se den un ejemplo de como funcionan las funciones
-#de get_Text y set_Text
-'''
-def obtenerText():
-    text = scroll_text_widget.get_text()
-    msg=messagebox.showinfo("Contenido",text+"")
-
-boton = Button(root,text="Hello", command=obtenerText)
-boton.grid(row=0,column=0)'''
-
-'''
-def PonerText():
-    new_text = "Este es el nuevo texto que quiero colocar en el ScrolledText."
-    scroll_text_widget.set_text(new_text)
-
-boton = Button(root,text="Hello", command=PonerText)
-boton.grid(row=0,column=0)'''
-
-#Este scroll es el que muestra mensajes o errores que surjan durante los analisis
-scrollAnalisis = ScrolledText(root, width=100,  height=8, font = ("Console", 11), state="disabled")
-scrollAnalisis.grid(row=2,column=0,padx=10,pady=10)
+scrollAnalisis = ScrolledText(root, width=100,  height=8, font = cambiar_tamaño_letra, state="disable")
+scrollAnalisis.grid(row=5,column=0,padx=10,pady=10)
 
 def cambiar_tamaño_letra(size):
     scroll_text_widget.text_widget.config(font=("Console", size))
+    
 
-#El menu bar donde estarán funciones de manejo de archivos y otras cosas
 menubar = Menu(root, background='#ff8000', foreground='black', activebackground='white', activeforeground='black')  
 file = Menu(menubar, tearoff=1)  
-file.add_command(label="New",command=NuevoArchivo)  
+file.add_command(label="New",command=NuevoArchivo)
 file.add_command(label="Open",command=AbrirArchivos)  
 file.add_command(label="Save", command=Guardar)  
 file.add_command(label="Save as", command=GuardarComo)    
 file.add_separator()  
-file.add_command(label="Exit", command=root.quit)  
+file.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=file)  
 
 analisis = Menu(menubar, tearoff=0)   
 analisis.add_command(label="Lexico",command=analisisLexico)  
-analisis.add_command(label="Sintactico", command=analisisSintactico)  
-menubar.add_cascade(label="Analizar", menu=analisis)  
+analisis.add_command(label="Sintactico", command=analisisSintactico) 
+analisis.add_command(label="Semantico" ) 
+menubar.add_cascade(label="Analizar", menu=analisis)
+
 
 tablas = Menu(menubar,tearoff=0)
-tablas.add_command(label="Estatica")
+tablas.add_command(label="Estatica", command=tablaEstatica)
 tablas.add_command(label="Dinamica")
 menubar.add_cascade(label="Tablas", menu=tablas)
 
-# Menú para seleccionar el tamaño de la fuente No jala bien de momento
 font_menu = Menu(menubar, tearoff=0)
 font_menu.add_command(label="10", command=lambda: cambiar_tamaño_letra(10))
 font_menu.add_command(label="12", command=lambda: cambiar_tamaño_letra(12))
